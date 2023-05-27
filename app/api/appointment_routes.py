64a2219cd0c63368e78e63069b255a7bc7cc1efe
Paul Jha <paul.hutalla@gmail.com>
2023-05-27 12:27:52 -0700

    added api

---
+++ app/api/appointment_routes.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from app.models import Appointment
+
+appointment_routes = Blueprint('appointments', __name__)
+
+@appointment_routes.route('')
+@login_required
+def get_all_appointments():
+    print(current_user)
+    appointments = Appointment.query.all()
+    return [appointment.to_appointment_dic() for appointment in appointments]
