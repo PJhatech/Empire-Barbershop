5fd914ffce9b53df74239375d27c725097852b9c
Paul Jha <paul.hutalla@gmail.com>
2023-05-31 15:52:21 -0700

    added appointment route

--- app/api/appointment_routes.py
+++ app/api/appointment_routes.py
@@ -1,12 +1,39 @@
-# from flask import Blueprint, jsonify, request
-# from flask_login import login_required, current_user
-# from app.models import Appointment
+from flask import Blueprint, jsonify, request, db
+from flask_login import login_required, current_user
+from flask_login import current_user, login_required
+from app.models import Appointment, barber_id, service_id
 
-# appointment_routes = Blueprint('appointments', __name__)
+appointment_routes = Blueprint('appointments', __name__)
 
-# @appointment_routes.route('')
-# @login_required
-# def get_all_appointments():
-#     print(current_user)
-#     appointments = Appointment.query.all()
-#     return [appointment.to_appointment_dic() for appointment in appointments]
+@appointment_routes.route('')
+@login_required
+def get_available_appointments():
+    print(current_user)
+    appointments = Appointment.query.all()
+    return [appointment.to_appointment_dic() for appointment in appointments]
+
+@appointment_routes.routes('/', methods=['GET', 'POST'])
+@login_required
+def handle_appointments():
+    if request.method == 'GET':
+        barber_appointments = Appointment.query.filter_by(barber_id.id).first()
+        if barber_appointments is None:
+            pass
+        else:
+            return barber_appointments.to_appointment_dict()
+
+    if request.method == 'POST':
+        data = request.json
+        barber_appointments = Appointment(
+            barber_id=barber_id.id,
+            client_id=current_user.id,
+            service_id=service_id.id,
+            date=data['date'],
+            time=data['time'],
+            repeat=data['repeat'],
+            created_at=data['created_at'],
+            updated_at=data['updated_at'],
+        )
+        db.session.add(barber_appointments)
+        db.session.commit()
+        return jsonify(barber_appointments.to_appointment_dic()), 201
---
+++ app/api/barber_routes.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from app.models import Barber
+
+barber_routes = Blueprint('barbers', __name__)
+
+@barber_routes.route('/')
+@login_required
+def barbers():
+    """
+    Query for all barbers and returns them in a list of barber dictionaries
+    """
+    barbers = Barber.query.all()
+    return {'barbers': [barber.to_barber_dict() for barber in barbers]}
+
+@barber_routes.route('/<int:id>')
+@login_required
+def barber(id):
+    """
+    Query for a barber by id and returns that barber in a dictionary
+    """
+    barbers = Barber.query.get(id)
+    return barber.to_barber_dict()
+
+def get_barbers():
+    print(current_user)
+    barbers = Barber.query.all()
+    return [barber.to_barber_dic() for barber in barbers]
+
+@barber_routes.route('/<int:id>')
+@login_required
+def get_barber_by_id(id):
+    stock = Barber.query.get(id)
+    return barber.to_barber_dict()
