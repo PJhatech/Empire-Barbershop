edeb6dba92d7e089c349201dcdfda19d2a285dfb
Paul Jha <paul.hutalla@gmail.com>
2023-06-07 16:51:24 -0700

    should be on frontend branch but not an issue. Made client user work and signed in

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, session, request
-from app.models import User, db
+from app.models import User, db, Client, Barber
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -24,7 +24,8 @@ def authenticate():
     Authenticates a user.
     """
     if current_user.is_authenticated:
-        return current_user.to_dict()
+        return current_user.to_client_dict()
+        # or current_user.to_barber_dict()
     return {'errors': ['Unauthorized']}
 
 
@@ -39,9 +40,12 @@ def login():
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         # Add the user to the session, we are logged in!
-        user = User.query.filter(User.email == form.data['email']).first()
-        login_user(user)
-        return user.to_dict()
+        client = Client.query.filter(Client.email == form.data['email']).first()
+        # barber = Barber.query.filter(Barber.email == form.data['email']).first()
+        login_user(client)
+        # login_user(barber)
+        return client.to_client_dict()
+        # or barber.to_barber_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
-from app.models import User
+from app.models import User, Client, Barber
 
 
 def user_exists(form, field):
-    # Checking if user exists
+    # Checking if client exists
     email = field.data
-    user = User.query.filter(User.email == email).first()
-    if not user:
+    client = Client.query.filter(Client.email == email).first()
+    # barber = Barber.query.filter(Barber.email == email).first()
+    if not client:
         raise ValidationError('Email provided not found.')
 
 
@@ -16,11 +17,16 @@ def password_matches(form, field):
     # Checking if password matches
     password = field.data
     email = form.data['email']
-    user = User.query.filter(User.email == email).first()
-    if not user:
-        raise ValidationError('No such user exists.')
-    if not user.check_password(password):
+    client = Client.query.filter(Client.email == email).first()
+    # barber = Barber.query.filter(Barber.email == email).first()
+    if not client:
+        raise ValidationError('No such client exists.')
+    # if not barber:
+    #     raise ValidationError('No such barber exists.')
+    if not client.check_password(password):
         raise ValidationError('Password was incorrect.')
+    # if not barber.check_password(password):
+    #     raise ValidationError('Password was incorrect.')
 
 
 class LoginForm(FlaskForm):
