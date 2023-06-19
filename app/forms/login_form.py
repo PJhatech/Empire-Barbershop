cf7cde54f7a177006945964c4d1c60af2c3f28e6
Paul Jha <paul.hutalla@gmail.com>
2023-06-19 16:34:33 -0700

    added

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -1,17 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
-from app.models import User, Client, Barber
+from app.models import User
 
 
 def user_exists(form, field):
-    # Checking if client exists
+    # Checking if user exists
     email = field.data
-    # client = Client.query.filter(Client.email == email).first()
-    barber = Barber.query.filter(Barber.email == email).first()
-    # if not client:
-    #     raise ValidationError('Email provided not found.')
-    if not barber:
+    user = User.query.filter(User.email == email).first()
+    if not user:
         raise ValidationError('Email provided not found.')
 
 
@@ -19,18 +16,14 @@ def password_matches(form, field):
     # Checking if password matches
     password = field.data
     email = form.data['email']
-    # client = Client.query.filter(Client.email == email).first()
-    barber = Barber.query.filter(Barber.email == email).first()
-    # if not client:
-    #     raise ValidationError('No such client exists.')
-    if not barber:
-        raise ValidationError('No such barber exists.')
-    # if not client.check_password(password):
-    #     raise ValidationError('Password was incorrect.')
-    if not barber.check_password(password):
+    user = User.query.filter(User.email == email).first()
+    if not user:
+        raise ValidationError('No such user exists.')
+    if not user.check_password(password):
         raise ValidationError('Password was incorrect.')
 
 
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired(), password_matches])
+    password = StringField('password', validators=[
+                           DataRequired(), password_matches])
