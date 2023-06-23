b84a7e5c672db9e7adccc8ee6adb2d5a390e0935
Paul Jha <paul.hutalla@gmail.com>
2023-06-23 00:06:40 -0700

    update

--- react-app/src/components/ServiceDeleteModal/index.js
+++ react-app/src/components/ServiceDeleteModal/index.js
@@ -27,10 +27,10 @@ const DeleteService = (service) => {
                 Are you sure you want to delete this Service?
             </p>
             <button className="button-class-submit" type="submit" onClick={handleSubmit}>
-                Yes(Delete Spot)
+                Yes(Delete Service)
             </button>
             <button onClick={closeModal}>
-                No(Keep Spot)
+                No(Keep Service)
             </button>
         </div>
     )
