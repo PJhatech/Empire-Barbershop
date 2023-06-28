5681647a7927028d3b6f107bffe801a7b777445e
Paul Jha <paul.hutalla@gmail.com>
2023-06-27 19:05:08 -0700

    update

--- react-app/src/store/client.js
+++ react-app/src/store/client.js
@@ -1,10 +1,16 @@
 const GET_CLIENT = "client/GET_CLIENT";
 const ADD_CLIENT = "client/ADD_CLIENT";
+const GET_CLIENT_BY_ID = "client/GET_CLIENT_BY_ID"
 
 const getClient = (clients) => ({
 	type: GET_CLIENT,
 	clients,
 });
+
+const getClientById = (client) => ({
+	type: GET_CLIENT_BY_ID,
+	client
+})
 
 const addClient = (clients) => ({
 	type: ADD_CLIENT,
@@ -18,6 +24,16 @@ export const fetchClients = () => async (dispatch) => {
 		dispatch(getClient(clients));
 	}
 	// console.log("<-------FetchClients-------->", response);
+};
+
+export const fetchClientIndex = (id) => async (dispatch) => {
+	const response = await fetch(`/api/users/clients/${id}/`);
+	if (response.ok) {
+		const client = await response.json();
+		dispatch(getClientById(client));
+	}
+
+	console.log("<--------fetchBarberIndex-------->", response);
 };
 
 export const createClient = (client) => async (dispatch) => {
@@ -38,12 +54,17 @@ export default function clientReducer(state = initialState, action) {
 	let newState = {};
 	switch (action.type) {
 		case GET_CLIENT: {
-			newState = {...state}
-			action.clients.forEach(client => {
-				newState[client.id] = client
-			})
-            // newState[action.clients.id] = action.clients
-			return {...newState}
+			newState = {...state};
+			action.clients.forEach((client) => {
+				newState[client.id] = client;
+			});
+			// newState[action.clients.id] = action.clients
+			return {...newState};
+		}
+		case GET_CLIENT_BY_ID: {
+			newState = {...state};
+			newState[action.client.id] = action.client;
+			return {...newState};
 		}
 		case ADD_CLIENT: {
 			newState = {...state};
