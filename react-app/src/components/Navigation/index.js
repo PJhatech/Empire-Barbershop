0aad3191b21bf197cc54b652e87a80a828a1d457
Paul Jha <paul.hutalla@gmail.com>
2023-07-13 17:30:38 -0700

    updated css

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -3,11 +3,10 @@ import {NavLink, useLocation} from "react-router-dom";
 import {useSelector} from "react-redux";
 import ProfileButton from "./ProfileButton";
 import barberPole from "../Images/barberPoleLogo.JPG";
-import whitePole from "../Images/whitepole.PNG"
+import whitePole from "../Images/whitepole.PNG";
 import "./Navigation.css";
 
-function Navigation({ isLoaded }) {
-	
+function Navigation({isLoaded}) {
 	const sessionUser = useSelector((state) => state.session.user);
 	const location = useLocation();
 	let navColor = location.pathname === "/" ? "black" : "initial";
@@ -17,39 +16,34 @@ function Navigation({ isLoaded }) {
 			className="navigation-container"
 			style={{backgroundColor: navColor}}
 		>
-			<div className="nav-wrapper">
-			<div>
-				<NavLink exact to="/">
-					{location.pathname !== "/" ? (
-						<img
-							alt="poleIcon"
-							className="barberPole"
-							src={barberPole}
-						/>
-					) : <img
-							alt="poleIcon"
-							className="barberPole"
-							src={whitePole}
-						/>}
-				</NavLink>
-			</div>
-				<div className="home-icon">
-					<div className="shopSign">
-						{/* {location.pathname === "/" && (
-							<h2>Welcome to Empire BarberShop</h2>
-						)} */}
-					</div>
+			<div className="barberPole-wrapper">
+				<div>
+					<NavLink exact to="/">
+						{location.pathname !== "/" ? (
+							<img
+								alt="poleIcon"
+								className="barberPole"
+								src={barberPole}
+							/>
+						) : (
+							<img
+								alt="poleIcon"
+								className="barberPole"
+								src={whitePole}
+							/>
+						)}
+					</NavLink>
+				</div>
+				<div className="components">
+					<div className="shopName">
+					<h1> Empire BarberShop </h1>
 
-					<div className="components">
-						<h1> Empire BarberShop </h1>
-						<NavLink to="/barberprofile">
-							{isLoaded && (
-								<>
-									<ProfileButton user={sessionUser} />
-								</>
-							)}
-						</NavLink>
 					</div>
+					{isLoaded && (
+						<div className="userButton">
+							<ProfileButton user={sessionUser} />
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
