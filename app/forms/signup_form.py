de1515c1ff4c43ae7853bb7890fc7f16ae40733e
Paul Jha <paul.hutalla@gmail.com>
2023-07-17 20:08:28 -0700

    updated

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -22,20 +22,22 @@ def username_exists(form, field):
 
 
 def valid_phone_number(form, field):
-    if len(field.data) > 16:
+    if len(str(field.data)) > 11:
         raise ValidationError('Invalid phone number.')
+    if len(str(field.data)) <= 9:
+        raise ValidationError('Not enough numbers')
 
 
-
-def first_name(form, field):
+def validate_first_name(form, field):
     first_name = field.data
     if len(first_name.strip()) == 0:
-        raise ValidationError('F
+        raise ValidationError('First name is required.')
+    elif len(first_name) > 50:
         raise ValidationError('First name is too long.')
 
 
 
-def last_name(form, field):
+def validate_last_name(form, field):
     last_name = field.data
     if len(last_name.strip()) == 0:
         raise ValidationError('Last name is required.')
@@ -43,18 +45,23 @@ def last_name(form, field):
         raise ValidationError('Last name is too long.')
 
 
-def user_type(form, field):
+def validate_user_type(form, field):
     user_type = field.data
     valid_types = [1, 2]
     if user_type not in valid_types:
         raise ValidationError('User type is invalid.')
 
+def validate_password(form, field):
+    password = field.data
+    if len(str(field.data)) < 6:
+        raise ValidationError('Password must be at least 6 characters long')
+
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
-    first_name = StringField('firstName', validators=[DataRequired() first_name])
-    last_name = StringField('lastName', validators=[DataRequired(), last_name])
+    password = StringField('password', validators=[DataRequired(), validate_password])
+    first_name = StringField('firstName', validators=[DataRequired(), validate_first_name])
+    last_name = StringField('lastName', validators=[DataRequired(), validate_last_name])
     phone_number = IntegerField('phone number', validators=[DataRequired(), valid_phone_number])
-    user_type = IntegerField('user type', validators=[DataRequired(), user_type])
+    user_type = IntegerField('user type', validators=[DataRequired(), validate_user_type])
