ae61f3418f5117f4b56bdc13747fec1045701f64
Paul Jha <paul.hutalla@gmail.com>
2023-08-05 11:34:46 -0700

    created api routes for locations

--- app/api/location_routes.py
+++ app/api/location_routes.py
@@ -39,43 +39,47 @@ def get_location_by_id(id):
 def post_new_location():
    data = request.json
    new_location = Location(
-      address='114 Sunvalley Mall',
-      city='Concord',
-      state='California',
-      zipCode=94520,
-      country='United States',
-      lat=123445689,
-      lng=987654321,
-      name="Concord Location",
+      address=['114 Sunvalley Mall'],
+      city=['Concord'],
+      state=['California'],
+      zipCode=['94520'],
+      country=['United States'],
+      lat=['123445689'],
+      lng=['987654321'],
+      name=["Concord Location"]
    )
-   db.session.add(location)
+   db.session.add(new_location)
    db.session.commit()
-   return jsonify(location.to_location_dict()), 201
+   return jsonify(new_location.to_location_dict()), 201
    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
 @location_routes.route('/<int:location_id>', methods=['PUT'])
 @login_required
 def update_location(location_id):
-    data = request.json
-    location = location.query.get(location_id)
+   data = request.json
+   location = Location.query.get(location_id)
 
-    if not location:
-        return jsonify({'error': 'location not found'}), 404
-    location.location_name = data.get('location_name', location.location_name)
-    location.description = data.get('description', location.description)
-    location.price = data.get('price', location.price)
-    location.time_frame = data.get('time_frame', location.time_frame)
-    db.session.commit()
-    return jsonify(location.to_location_dict())
+   if not location:
+      return jsonify({'error': 'location not found'}), 404
+   location.address = data.get('address', location.address)
+   location.city = data.get('city', location.city)
+   location.state = data.get('state', location.state)
+   location.zipCode = data.get('zipCode', location.zipCode)
+   location.country = data.get('country', location.country)
+   location.lat = data.get('lat', location.lat)
+   location.lng = data.get('lng', location.lng)
+   location.name = data.get('name', location.name)
+   db.session.commit()
+   return jsonify(location.to_location_dict())
 
 
 @location_routes.route('/<int:id>', methods=['DELETE'])
 @login_required
 def delete_location(id):
-    location = location.query.get(id)
-    if location:
-        db.session.delete(location)
-        db.session.commit()
-        return jsonify({'message': 'location deleted successfully'}), 204
-    return jsonify({'message': 'location not found'}), 404
+   location = Location.query.get(id)
+   if location:
+      db.session.delete(location)
+      db.session.commit()
+      return jsonify({'message': 'location deleted successfully'}), 204
+   return jsonify({'message': 'location not found'}), 404
