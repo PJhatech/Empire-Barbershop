d6a6206dad3bd70e376e63460dcd45d49771926a
Paul Jha <paul.hutalla@gmail.com>
2023-08-05 12:01:23 -0700

    added hercules location seeder to location

--- app/seeds/location.py
+++ app/seeds/location.py
@@ -3,7 +3,7 @@ from sqlalchemy.sql import text
 
 
 def seed_location():
-    locationSeeder = Location(
+    concord_location = Location(
          address='114 Sunvalley Mall',
          city='Concord',
          state='California',
@@ -15,7 +15,20 @@ def seed_location():
     )
 
 
-    db.session.add(locationSeeder)
+    hercules_location = Location(
+         address='3700 San Pablo Ave',
+         city='Hercules',
+         state='California',
+         zipCode=94547,
+         country='United States',
+         lat=987654321,
+         lng=123445689,
+         name="Hercules Location",
+    )
+
+
+    db.session.add(concord_location)
+    db.session.add(hercules_location)
     db.session.commit()
 
 
