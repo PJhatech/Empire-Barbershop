da154de7e815d0aae555d4136ee648e456e7d9d4
Paul Jha <paul.hutalla@gmail.com>
2023-09-28 12:37:08 -0700

    testing calander

--- react-app/src/components/Appointments/index.js
+++ react-app/src/components/Appointments/index.js
@@ -42,7 +42,7 @@ const Appointments = () => {
 	const dispatch = useDispatch();
 	const appointmentReducer = useSelector((state) => state.appointmentReducer);
 	const closeModal = useModal();
-	const appointment = useMemo(() => Object.values(appointmentReducer), [appointmentReducer]);
+	const appointment = useMemo(() => Object.values(appointmentReducer), []);
 	const localizer = momentLocalizer(moment);
 
 	const [date, setDate] = useState();
